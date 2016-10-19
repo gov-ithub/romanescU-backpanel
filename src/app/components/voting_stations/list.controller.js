c20e9ba7583021f0522ad4e43a4aad452dd677f6
Liviu Ciulinaru <liviu@fullstack.ro>
2016-10-19 12:46:59 +0300

    pagination and search form updates

--- src/app/components/voting_stations/list.controller.js
+++ src/app/components/voting_stations/list.controller.js
@@ -8,25 +8,68 @@
         var vm = this;
         vm.delete = remove;
 
-        // vm.items = VotingStations.query();
+        init()
 
-        vm.items = [{
-          crt: 1,
-          country: "albania",
-          city: 'Tirana',
-          address: "24, Rue Arezki Abri , Hydra, 16035",
-          location: {
-            latitude: 33.23234,
-            longitude: 34.2332
-          }
-        }]
+        function init() {
+          //set default
+          vm.query = {};
+          vm.pagination = {
+            currentPage: 1,
+            itemsPerPage: 7,
+            //   totalItems: 150,
+            maxDisplayedPages: 5
+          };
 
+          vm.sortBy = 'crt';
+          vm.sortDirection = "ASC";
+          search();
+        }
 
+        function search() {
+          vm.items = VotingStations.query({
+            q: vm.query, // name = "test search"
+            currentPage: vm.pagination.currentPage, // 1
+            itemsPerPage: vm.pagination.itemsPerPage, // 50
+            sortBy: vm.sortBy, // name
+            sortDirection: vm.sortDirection // 'ASC' sau 'DESC'
+          });
+
+          //TODO delete when the server is running
+          vm.items = [{
+            crt: 1,
+            country: "albania",
+            city: 'Tirana',
+            address: "24, Rue Arezki Abri , Hydra, 16035",
+            location: {
+              latitude: 33.23234,
+              longitude: 34.2332
+            }
+          }, {
+            crt: 2,
+            country: "alb3234f3ania",
+            city: 'Tirana',
+            address: "24, Rue Arezki Abri , Hydra, 16035",
+            location: {
+              latitude: 33.23234,
+              longitude: 34.2332
+            },
+            isEnabled: true
+          }, {
+            crt: 3,
+            country: "asdasdasd",
+            city: 'Tirana',
+            address: "24, Rue Arezki Abri , Hydra, 16035",
+            location: {
+              latitude: 33.23234,
+              longitude: 34.2332
+            },
+            isEnabled: false
+          }]
+        }
 
         function remove() {
 
         }
-
 
         $scope.$on("$destroy", function () {
           vm.items = [];
